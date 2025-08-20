c5a5d80cdc0edf33b0e63f5fca170ae536b24528
Erlend Johann Skarpnes <erlend.skarpnes@miles.no>
2025-08-20 15:38:20 +0200

    theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,13 +7,7 @@
 	let activeUrl = $derived(page.url.pathname);
 
 	const theme: ThemeConfig = {
-		card: {
-			base: "bg-gray-100 dark:bg-background-400 dark:border-background-500"
-		},
-		navbar: {
-			base: "bg-gray-100 dark:bg-gray-700"
-		},
-		heading: "medievalsharp-regular dark:text-amber-500 "
+		heading: "medievalsharp-regular text-amber-700 dark:text-amber-500"
 	};
 
 	// Navigation items
@@ -29,19 +23,19 @@
 	</svelte:head>
 
 <ThemeProvider theme={theme}>
-	<div class="bg-gray-200 dark:bg-background-500 min-h-full">
+	<div class="bg-gray-100 dark:bg-gray-900 min-h-full">
 		<header>
 			<Navbar>
 				<NavBrand href="/">
 					<Heading class="text-2xl">World of Noobs</Heading>
 				</NavBrand>
 				<NavHamburger />
-				<NavUl {activeUrl}>
+				<NavUl {activeUrl} classes={{ active: "md:dark:text-amber-500", nonActive: "md:hover:dark:text-amber-500" }}>
 					{#each navItems as item}
 						<NavLi href={item.href}>{item.label}</NavLi>
 					{/each}
+					<DarkMode />
 				</NavUl>
-				<DarkMode />
 			</Navbar>
 		</header>
 
--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -15,7 +15,7 @@
 			<Heading class="text-2xl text-center mb-4 border-b pb-2">Highest Level</Heading>
 			<div class="space-y-2">
 				{#each data.stats.level as line, index}
-					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-background-300' : ''}">
+					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-gray-700' : ''}">
 						<div class="flex items-center">
 							<P class="font-medium">{line.name}</P>
 						</div>
@@ -29,7 +29,7 @@
 			<Heading class="text-2xl text-center mb-4 border-b pb-2">Longest Playtime</Heading>
 			<div class="space-y-2">
 				{#each data.stats.playtime as line, index}
-					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-background-300' : ''}">
+					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-gray-700' : ''}">
 						<div class="flex items-center">
 							<P class="font-medium">{line.name}</P>
 						</div>
@@ -43,7 +43,7 @@
 			<Heading class="text-2xl text-center mb-4 border-b pb-2">Most Money</Heading>
 			<div class="space-y-2">
 				{#each data.stats.money as line, index}
-					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-background-300' : ''}">
+					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-gray-700' : ''}">
 						<div class="flex items-center">
 							<P class="font-medium">{line.name}</P>
 						</div>
@@ -57,7 +57,7 @@
 			<Heading class="text-2xl text-center mb-4 border-b pb-2">Most Quests</Heading>
 			<div class="space-y-2">
 				{#each data.stats.quests as line, index}
-					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-background-300' : ''}">
+					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-gray-700' : ''}">
 						<div class="flex items-center">
 							<P class="font-medium">{line.name}</P>
 						</div>
@@ -71,7 +71,7 @@
 			<Heading class="text-2xl text-center mb-4 border-b pb-2">Highest Profession</Heading>
 			<div class="space-y-2">
 				{#each data.stats.profession as line, index}
-					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-background-300' : ''}">
+					<div class="flex flex-row justify-between items-center p-2 rounded {index % 2 === 0 ? 'bg-gray-50 dark:bg-gray-700' : ''}">
 						<div class="flex items-center">
 							<P class="font-medium">{line.name}</P>
 						</div>
