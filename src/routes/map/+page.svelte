7ce3b09dad1ed44376d6bbc0caddf744e5ae0e0f
Erlend Johann Skarpnes <erlend.skarpnes@miles.no>
2025-12-29 16:00:56 +0100

    cleanup

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -3,26 +3,6 @@
 	import DeckGLMap from '$lib/components/DeckGLMap.svelte';
 
 	let { data }: { data: { players: AccountOverviewData<CharacterDataWithCoordinates>[] } } = $props();
-
-	let showOffline = $state(false);
-
-	let players = $derived.by(() => {
-
-			let allPlayers = data.players.flatMap(player => player.characters).filter(player => player.level > 1) as CharacterDataWithCoordinates[];
-
-			if (showOffline === false) {
-				return allPlayers.filter(player => player.online === true);
-			}
-			return allPlayers.sort((a, b) => {
-				if (a.online === b.online) {
-					return b.lastOnline - a.lastOnline;
-				}
-				return a.online ? -1 : 1;
-			});
-		}
-	);
-
-
 </script>
 
 <DeckGLMap players={data.players.flatMap(player => player.characters) ?? []} />
